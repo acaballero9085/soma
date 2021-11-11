116dc347b4a243e97cd9f8ff117deb1cbbb46ec9
Arturo Caballero <acaballero9085@gmail.com>
2021-11-10 20:54:25 -0600

    first commit

---
+++ pages/Home/HomeBanner.js
@@ -0,0 +1,24 @@
+import styles from './HomeBanner.module.css'
+import { Container, Row, Col } from 'react-grid-system';
+import { Text } from '../styled-components'
+
+const HomeBanner = () => (
+  <Container fluid className={styles.container}>
+    <Row justify="center">
+      <Col sm="content">
+        <Text variant="h1"><i>Soma</i></Text>
+      </Col>
+    </Row>
+    <Row>
+      <Col sm={12}></Col>
+    </Row>
+    <Row justify="center">
+      <Col sm="content">HOME</Col>
+      <Col sm="content">OUR MENUS</Col>
+      <Col sm="content">BLOG ENTRIES</Col>
+      <Col sm="content">CONTACT US</Col>
+    </Row>
+  </Container>
+);
+
+export default HomeBanner
---
+++ pages/styled-components/Text.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components'
+
+const fontMap = {
+  h1: '48px',
+  h2: '22px',
+  h3: '17px',
+  p: '13px',
+}
+
+const colorMap = {
+  textDark: '#2c343b',
+  textLight: '#fff',
+  textPrimary: '#f2745f',
+}
+
+const TextWrapper = styled.span`
+  color: ${props => colorMap[props.color] || '#2c343b'};
+  font-size: ${props => fontMap[props.variant] || '22px'};
+`
+
+const Text = (props) => (
+  <TextWrapper {...props}>
+    {props.children}
+  </TextWrapper>
+)
+
+export default Text
